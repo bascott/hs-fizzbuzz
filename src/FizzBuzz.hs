0366b47d42e6ee7d162fbdf85a24d2896bd9b87f
Brian Scott <briananthonyscott@gmail.com>
2016-10-17 14:33:22 -0400

    Factor out code

--- src/FizzBuzz.hs
+++ src/FizzBuzz.hs
@@ -9,14 +9,16 @@ fizzbuzz :: ListT IO ()
 fizzbuzz = fizzbuzz' [1..]
 
 fizzbuzz' :: [Int] -> ListT IO ()
-fizzbuzz' xs
-    = do x <- (ListT . pure) xs
-         is3 <- if mod x 3 == 0
-                   then liftIO $ putStr "fizz" >> pure True
-                   else pure False
-         is5 <- if mod x 5 == 0
-                   then liftIO $ putStr "buzz" >> pure True
-                   else pure False
-         liftIO $ putStrLn $ if is3 || is5
-                                then ""
-                                else show x
+fizzbuzz' xs = (ListT . pure . fizzbuzz'') xs >>= liftIO . putStrLn
+
+fizzbuzz'' :: [Int] -> [String]
+fizzbuzz'' xs
+    = do x <- xs
+         let fizz = if mod x 3 == 0 then "fizz" else ""
+         let buzz = if mod x 5 == 0 then "buzz" else ""
+         pure $ (fizz ++ buzz) <++ show x 
+  where
+      infixr 5 <++
+      [] <++ xs = xs
+      xs <++ _  = xs
+ 
